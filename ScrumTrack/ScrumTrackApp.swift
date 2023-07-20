6b20bda0754d5972702773c5cf3f75bca815a9be
Nathan Hannon <nathan@hannon.me>
2023-07-20 18:28:38 -0400

    Initial Commit

---
+++ ScrumTrack/ScrumTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumTrackApp.swift
+//  ScrumTrack
+//
+//  Created by Nathan Hannon on 2023-07-20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
