eab56364abf3a68e6ac30e2b96d30846ede1a600
Nathan Hannon <nathan@hannon.me>
2023-10-09 13:57:15 -0400

    Added edit view

---
+++ ScrumTrack/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  ScrumTrack
+//
+//  Created by Nathan Hannon on 2023-10-09.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
---
+++ ScrumTrack/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ScrumTrack
+//
+//  Created by Nathan Hannon on 2023-10-09.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
